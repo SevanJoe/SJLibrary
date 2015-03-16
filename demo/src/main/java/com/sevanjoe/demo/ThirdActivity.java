00dd07e9580ab6780a1844e3cdb4bb574919cd31
Sevan Joe <sevancr7@gmail.com>
2015-03-16 15:09:03 +0800

    add ThirdActivity

--- demo/src/main/java/com/sevanjoe/demo/ThirdActivity.java
+++ demo/src/main/java/com/sevanjoe/demo/ThirdActivity.java
@@ -16,26 +16,26 @@
 
 package com.sevanjoe.demo;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class SecondActivity extends ActionBarActivity {
+
+public class ThirdActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_second, menu);
+        getMenuInflater().inflate(R.menu.menu_third, menu);
         return true;
     }
 
@@ -55,6 +55,7 @@ public class SecondActivity extends ActionBarActivity {
     }
 
     public void testCrash(View view) {
-        startActivity(new Intent(this, ThirdActivity.class));
+        String string = null;
+        System.out.print(string.equals("test"));
     }
 }
